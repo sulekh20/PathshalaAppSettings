2d01315e429adb32d40d88b35985e9b713626cc6
Sulekh Ranjan <Sulekh Ranjan@LAPTOP-7S6V2D42.bbrouter>
2021-04-17 22:35:45 +0530

    New git project

---
+++ PathshalaAppSettings/src/main/java/com/app/pathsala/Pathsala/PathsalaApplication.java
@@ -0,0 +1,13 @@
+package com.app.pathsala.Pathsala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathsalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathsalaApplication.class, args);
+	}
+
+}
---
+++ PathshalaAppSettings/src/test/java/com/app/pathsala/Pathsala/PathsalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pathsala.Pathsala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathsalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
